536bfc22f05654f726c3271e5b37f3b957c1940e
Konnokai <f20001012@gmail.com>
2022-08-31 00:11:14 +0800

    add: PubSubHubBub 收到 Deleted 事件時的處理 misc: 修改 PubSubHubBub 的顯示方式

--- Discord Stream Bot Backend/Controllers/YouTubeNotificationsController.cs
+++ Discord Stream Bot Backend/Controllers/YouTubeNotificationsController.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.IO;
 using System.Xml;
+using Newtonsoft.Json;
 
 namespace Discord_Stream_Bot_Backend.Controllers
 {
@@ -17,7 +18,6 @@ namespace Discord_Stream_Bot_Backend.Controllers
             _logger = logger;
         }
 
-        //原則上不須處理Deleted的影片，小幫手會自動處理 (暫時)
         [HttpGet]
         [HttpPost]
         public ContentResult NotificationCallback([FromQuery(Name = "hub.topic")] string topic, [FromQuery(Name = "hub.challenge")] string challenge, [FromQuery(Name = "hub.mode")] string mode, [FromQuery(Name = "hub.lease_seconds")] string leaseSeconds)
@@ -30,26 +30,9 @@ namespace Discord_Stream_Bot_Backend.Controllers
                     {
                         var data = ConvertAtomToClass(Request.Body);
                         if (data == null)
-                            return Content("200");
+                            return Content("400");
 
-                        if (data.IsNewVideo)
-                        {
-                            _logger.LogInformation($"New Video!\n" +
-                                $"Title: {data}\n" +
-                                $"Video Id: {data.VideoId}\n" +
-                                $"Channel Id: {data.ChannelId}\n" +
-                                $"Published: {data.Published}");
-                        }
-                        else
-                        {
-                            _logger.LogInformation($"Video Update!\n" +
-                               $"Title: {data.Title}\n" +
-                               $"Video Id: {data.VideoId}\n" +
-                               $"Channel Id: {data.ChannelId}\n" +
-                               $"Published: {data.Published}\n" +
-                               $"Updated: {data.Updated}");
-                        }
-
+                        _logger.LogInformation(data.ToString());
                         return Content("200");
                     }
                     catch (Exception ex)
@@ -86,54 +69,92 @@ namespace Discord_Stream_Bot_Backend.Controllers
         //https://ithelp.ithome.com.tw/articles/10186605
         private YoutubeNotification ConvertAtomToClass(Stream stream)
         {
-            string xmlText = new StreamReader(stream).ReadToEnd();
-            //_logger.LogInformation(xmlText);
-            if (xmlText.Contains("deleted-entry"))
+            try
+            {
+                var youtubeNotification = new YoutubeNotification();
+                string xmlText = new StreamReader(stream).ReadToEnd();
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(xmlText);
+
+                if (xmlText.Contains("https://www.youtube.com/xml/feeds/videos.xml?channel_id"))
+                {
+                    youtubeNotification.VideoId = doc.GetElementsByTagName("yt:videoId")[0]?.InnerText;
+                    youtubeNotification.ChannelId = doc.GetElementsByTagName("yt:channelId")[0]?.InnerText;
+                    youtubeNotification.Title = doc.GetElementsByTagName("title")[1]?.InnerText;
+                    youtubeNotification.Link = doc.GetElementsByTagName("link")[0]?.Attributes["href"].Value;
+                    youtubeNotification.Published = (DateTime)(doc.GetElementsByTagName("published")[0]?.InnerText.ConvertDateTime());
+                    youtubeNotification.Updated = (DateTime)(doc.GetElementsByTagName("updated")[0]?.InnerText.ConvertDateTime());
+                    youtubeNotification.NotificationType = YoutubeNotification.YTNotificationType.CreateOrUpdated;
+
+                    Utility.RedisSub.Publish("youtube.pubsub.update", JsonConvert.SerializeObject(youtubeNotification), StackExchange.Redis.CommandFlags.FireAndForget);
+                }
+                else if (xmlText.Contains("deleted-entry"))
+                {
+                    var node = doc.GetElementsByTagName("at:deleted-entry")[0];
+                    if (node == null)
+                        return null;
+
+                    youtubeNotification.VideoId = node.Attributes.GetNamedItem("ref").Value.Split(new char[] { ':' })[2];
+                    youtubeNotification.ChannelId = doc.GetElementsByTagName("uri")[0]?.InnerText;
+                    youtubeNotification.ChannelId = youtubeNotification.ChannelId.Substring(youtubeNotification.ChannelId.Length - 24, 24);
+                    youtubeNotification.Published = (DateTime)node.Attributes.GetNamedItem("when").Value.ConvertDateTime();
+                    youtubeNotification.NotificationType = YoutubeNotification.YTNotificationType.Deleted;
+
+                    Utility.RedisSub.Publish("youtube.pubsub.deleted", JsonConvert.SerializeObject(youtubeNotification), StackExchange.Redis.CommandFlags.FireAndForget);
+                }
+                else
+                {
+                    _logger.LogWarning("未知的Atom");
+                    _logger.LogWarning(xmlText);
+                    return null;
+                }
+
+                return youtubeNotification;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "ConvertAtomToClass錯誤\n");
                 return null;
-
-            var YoutubeNotification = new YoutubeNotification();
-            XmlDocument doc = new XmlDocument();
-            doc.LoadXml(xmlText);
-            YoutubeNotification.VideoId = doc.GetElementsByTagName("yt:videoId")[0]?.InnerText;
-            YoutubeNotification.ChannelId = doc.GetElementsByTagName("yt:channelId")[0]?.InnerText;
-            YoutubeNotification.Title = doc.GetElementsByTagName("title")[1]?.InnerText;
-            YoutubeNotification.Link = doc.GetElementsByTagName("link")[0]?.Attributes["href"].Value;
-            YoutubeNotification.Published = doc.GetElementsByTagName("published")[0]?.InnerText;
-            YoutubeNotification.Updated = doc.GetElementsByTagName("updated")[0]?.InnerText;
-
-            return YoutubeNotification;
-
-
-            //Regex regex = new Regex(@"<at:deleted-entry ref=""yt:video:(?'VideoId'[\w\-\\_]{11})"" when=""(?'Date'\d{4}-\d{2}-\d{2})T(?'Time'\d{2}:\d{2}:\d{2})");
-            //if (regex.IsMatch(xmlText))
-            //{
-            //    var regexResult = regex.Match(xmlText);
-            //    youtubeNotification = new YoutubeNotification()
-            //    {
-            //        VideoId = regexResult.Groups["VideoId"].Value,
-            //        Published = Convert.ToDateTime($"{regexResult.Groups["Date"].Value} {regexResult.Groups["Time"].Value}").AddHours(8),
-            //        NotificationType = NotificationType.Deleted
-            //    };
-            //}
-            //else
-            //{
-            //}
+            }
         }
     }
 
     public class YoutubeNotification
     {
+        public enum YTNotificationType { CreateOrUpdated, Deleted }
+
+        public YTNotificationType NotificationType { get; set; } = YTNotificationType.CreateOrUpdated;
         public string VideoId { get; set; }
         public string ChannelId { get; set; }
         public string Title { get; set; }
         public string Link { get; set; }
-        public string Published { get; set; }
-        public string Updated { get; set; }
-        public bool IsNewVideo
+        public DateTime Published { get; set; }
+        public DateTime Updated { get; set; }
+
+        public override string ToString()
         {
-            get
+            switch (NotificationType)
             {
-                return Published == Updated && !string.IsNullOrEmpty(Published);
+                case YTNotificationType.CreateOrUpdated:
+                    return $"({NotificationType} at {Updated}) {ChannelId} - {VideoId} | {Title}";
+                case YTNotificationType.Deleted:
+                    return $"({NotificationType} at {Published}) {ChannelId} - {VideoId}";
+            }
+            return "";
+        }
+    }
+
+    public static class Ext
+    {
+        public static DateTime? ConvertDateTime(this string text)
+        {
+            try
+            {
+                return Convert.ToDateTime(text);
+            }
+            catch 
+            {
+                return new DateTime();
             }
         }
     }
