7708e9e2a4f0c53802a0ac8d9be52fc6b21be13f
Konnokai <f20001012@gmail.com>
2022-09-07 01:12:42 +0800

    add: 錯誤請求檢測

--- Discord Stream Bot Backend/Middleware/LogMiddleware.cs
+++ Discord Stream Bot Backend/Middleware/LogMiddleware.cs
@@ -25,10 +25,37 @@ namespace Discord_Stream_Bot_Backend.Middleware
             try
             {
                 var remoteIpAddress = context.GetRemoteIPAddress();
-                var requestUrl = context.Request.GetDisplayUrl(); 
-                logger.Info($"({remoteIpAddress}) {context.Request.Method} {requestUrl}");
+                var requestUrl = context.Request.GetDisplayUrl();
+                string redisKey = $"server.errorcount:{remoteIpAddress.ToString().Replace(":", "-").Replace(".", "-")}";
+
+                if (!context.Request.Headers.TryGetValue("Content-Type", out var contentType) || contentType != "application/atom+xml")
+                {
+                    var badCount = await Utility.RedisDb.StringGetAsync(redisKey);
+                    if (badCount.HasValue && int.Parse(badCount.ToString()) >= 5)
+                    {
+                        await Utility.RedisDb.StringIncrementAsync(redisKey);
+                        await Utility.RedisDb.KeyExpireAsync(redisKey, TimeSpan.FromHours(1));
+                        var errorMessage = JsonConvert.SerializeObject(new
+                        {
+                            ErrorMessage = "429 Too Many Requests"
+                        });
+                        var bytes = Encoding.UTF8.GetBytes(errorMessage);
+
+                        context.Response.StatusCode = 429;
+                        await originalResponseBodyStream.WriteAsync(
+                            bytes, 0, bytes.Length);
+                        return;
+                    }
+                }
 
                 await _next(context);
+
+                logger.Info($"{remoteIpAddress} | {context.Request.Method} | {context.Response.StatusCode} | {requestUrl}");
+                if (context.Response.StatusCode >= 400 && context.Response.StatusCode < 500)
+                {
+                    await Utility.RedisDb.StringIncrementAsync(redisKey);
+                    await Utility.RedisDb.KeyExpireAsync(redisKey, TimeSpan.FromHours(1));
+                }
             }
             catch (Exception e)
             {
