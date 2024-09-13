b1f2850df8875393aa7d256ff68454f1fc5d3681
孤之界 <f20001012@gmail.com>
2024-09-13 11:33:25 +0800

    feat: 請求 index 時自動轉址到小幫手的首頁

---
+++ Discord Stream Bot Backend/Controllers/IndexController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Discord_Stream_Bot_Backend.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class IndexController : Controller
+    {
+        [EnableCors("allowGET")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Redirect("https://dcbot.konnokai.me/stream");
+        }
+    }
+}
